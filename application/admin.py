85536deae7d078de1b4b81372ad9d4cd67db16f9
Bozidar Spirovski <spirovski.b@gmail.com>
2020-07-22 14:13:26 +0200

    admin interface update

--- application/admin.py
+++ application/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from application.models import Post, Project
+from application.models import Post, Project, Note
 
 
 #START: this is the view for the Challenge model area in admin
@@ -13,3 +13,7 @@ admin.site.register(Post,PostModelAdmin)
 class ProjectModelAdmin(admin.ModelAdmin):
     list_display = ('project_name','project_active','project_start_date','project_end_date')
 admin.site.register(Project,ProjectModelAdmin)
+
+class NoteModelAdmin(admin.ModelAdmin):
+    list_display = ('note_title','note_author')
+admin.site.register(Note,NoteModelAdmin)
