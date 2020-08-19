aa84a904da55b942f197e9c253d0e95314975a3c
Bozidar Spirovski <spirovski.b@gmail.com>
2020-08-19 08:45:09 +0200

    CodeQL review

--- env_test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-DEBUG = os.getenv('DJANGO_DEBUG', True)
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
-DB_PATH = os.getenv('DJANGO_DATABASE_PATH')
-STATIC_FILE_PATH = os.getenv('DJANGO_STATIC_PATH')
-MEDIA_FILE_PATH = os.getenv('DJANGO_MEDIA_PATH')
